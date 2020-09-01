6c5f40ab375fa63187230235832b5266ee424e76
mauricechouam <lansichouamou@gmail.com>
2020-08-31 22:23:40 -0700

    Updated Css File for Navbar and Header pages

--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -1,14 +1,24 @@
 import React from "react";
-import style from "./style.css";
+import "./style.css";
+
 
 function Header() {
     return (
         <div className="jumbotron text-center">
             <div className="container text-light">
-                <h1>Google Books Search</h1>
+                <h1 class="ml11">
+                    <span class="text-wrapper">
+                        <span class="line line1"></span>
+                        <span class="letters">Google Books Search</span>
+                    </span>
+                </h1>
             </div>
+
         </div>
+
     );
+
 }
+
 
 export default Header;
